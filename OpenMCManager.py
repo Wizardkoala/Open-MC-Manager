0afab6f1624348bfb57af32b2978393fb4327822
Wizardkoala <50385897+Wizardkoala@users.noreply.github.com>
2020-02-25 12:11:58 -0500

    Add files via upload

--- OpenMCManager.py
+++ OpenMCManager.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Made by Benjamin Hamilton | 2020
 # For questions and concerns please email Wizardkoala31@gmail.com
-# Made with Atom
 
 ### THIS IS IN ALPHA !!! ###
 import os, pickle, shutil, easygui
@@ -12,7 +11,7 @@ print("This project is NOT made by nor affiliated with Mojang.")
 print("This window is a console do NOT close or input anything into this window!")
 print("You can minimize this window however.")
 Menu = True
-
+debug = False
 
 #Load the list of known built servers
 def LoadNames():
@@ -49,7 +48,8 @@ def Build():
     print("If you have problems try installing a JRE before useing")
     #Creates Dir and adds to Name list
     Name = easygui.enterbox("Name the server!")
-    os.system("md \"Servers\\"+Name+"\\\"")
+    Name = Name.replace(" ", "_")
+    os.system("md Servers\\"+Name+"\\")
     AddName(Name)
 
     #Loads the versions in names format eg. 1.15.2
@@ -96,21 +96,22 @@ def Build():
         open("Servers\\"+Name+"\\eula.txt", 'w').write(E)
     else:
         print("Stopping Build")
-        shutil.rmtree("Servers\\"+Name, ignore_errors=False, onerror=handleRemoveReadonly)
+        shutil.rmtree("Servers\\"+Name+"", ignore_errors=False, onerror=handleRemoveReadonly)
+def RemoveName(Name):
+    Names = LoadNames()
+    Names.remove(Name)
 
-def Remove():
-    Name = easygui.buttonbox("Which sever do you want to remove?", 'Open MC Manager', LoadNames())
-    Confirm = easygui.ynbox("Are you sure you want to remove "+Name, 'Open Mc Manager')
+    file = open("Servers.list", 'wb')
+    pickle.dump(Names, file)
+    file.close()
+
+def Remove(Name):
+    Confirm = easygui.ynbox("Are you sure you want to remove: "+Name, 'Open Mc Manager')
     if Confirm:
         try:
             shutil.rmtree("Servers\\"+Name, ignore_errors=False, onerror=handleRemoveReadonly)
+            RemoveName(Name)
 
-            Names = LoadNames()
-            Names.remove(Name)
-
-            file = open("Servers.list", 'wb')
-            pickle.dump(Names, file)
-            file.close()
         except:
             Names = LoadNames()
             Names.remove(Name)
@@ -124,50 +125,118 @@ def Remove():
 
 
 #Open the dir of Name for changes that cant be made in the manager
-def Openfiles():
+def Openfiles(Name):
+    os.system("start Servers\\"+Name+"")
 
-    d = easygui.buttonbox("Which server?", 'Open MC Manager', LoadNames())
-    os.system("start Servers\\"+d)
+#Starts Name
+def Start(Name):
+    easygui.msgbox("Type stop to shutdown the server.", "Open MC Manager")
+    #Starts server
+    os.system("start Servers\\"+Name+"\\Run.bat")
 
-#Starts name
-def Start():
-    #Checks if there is a server to start.
-    if LoadNames() == []:
-        easygui.msgbox("Please create a server first!", "Open MC Manager")
-        sleep(2)
-        return False
-    else:
-        Name = easygui.buttonbox("Pick a server to start", "Open MC Manager",LoadNames())
-        easygui.msgbox("Type \"stop\" to shutdown the server.", "Open MC Manager")
-        #Starts server
-        os.system("start Servers\\"+Name+"\\Run.bat")
-        return True
+def Import():
+    #Askes for the path of the server to import
+    Path = easygui.diropenbox("Open MC Manager")
+    Name = os.path.basename(os.path.normpath(Path))
 
+    Name = Name.replace(" ", "_")
+    os.system("move "+Path+" Servers\\")
+    os.system("ren \"Servers\\"+Name+"\" "+Name.replace(" ", "_"))
+
+    MaxRam = str(int(easygui.enterbox("How many gigabytes of ram do you want to dedicate?")))
+    data = "echo off\ncls\ncd Servers\\"+Name+"\njava -Xmx"+MaxRam+"G -Xms1G -jar server.jar nogui\nexit"
+    open("Servers\\"+Name+"\\Run.bat", 'w').write(data)
+
+    AddName(Name)
+    easygui.msgbox("Server Imported!", "Open MC Manager")
+
+def Rename(Name):
+    NewName = easygui.enterbox("What do you want the new name to be?", "Open MC Manager").replace(" ", "_")
+
+    os.system("ren Servers\\"+Name+" "+NewName)
+    print("Renamed: "+Name+" To "+NewName)
+
+    base = open("Servers\\"+NewName+"\\Run.bat", 'r').readlines()
+    Data = ""
+    for line in base:
+        Data += line.replace(Name, NewName)
+    open("Servers\\"+NewName+"\\Run.bat", 'w').write(Data)
+
+
+    RemoveName(Name)
+    AddName(NewName)
+
+try:
+    debugSet = open("debug.txt", 'r').readlines()
+    NameEdits = False
+    debug = True
+    if "AllowNameEdits" in debugSet:
+        NameEdits = True
+except:
+    DoNothing = "Okay"
 #Makes sure there is a list and it is correctly setup
 try:
     LoadNames()
 except:
+    easygui.msgbox("This project is not made by nor affiliated with mojang!")
+    easygui.msgbox("You need java to be installed to use this!\nIf you have trouble try installing a JRE.", "Open MC Manager")
     file = open("Servers.list", 'wb')
     pickle.dump([], file)
     file.close()
 
+if debug:
+    if NameEdits:
+        Names = LoadNames()
+        cho = easygui.buttonbox("Debug mode is not stable!", "Open MC Manager: DEBUG", ["Add", "Delete"])
+        name = easygui.enterbox("", "Open MC Manager: DEBUG", Names)
+        if cho == "Delete":
+            RemoveName(name)
+        elif cho == "Add":
+            AddName(name)
+        else:
+            print("Failed")
+    else:
+        easygui.msgbox("No debug options selected!", "Open MC Manager: DEBUG")
+
 
 while Menu:
+    Names = LoadNames()
+
+    NamesRemoved = []
+    for name in Names:
+        NamesRemoved.append(name.replace("_", " "))
     #Gives a list of options
-    message = "Welcome to Open MC Manager!\nIf you are thinking, \"This GUI is crap...\" You're right. I'm not too good with GUIs, but if you are feel free to contribute on git hub or emailing Wizardkoala31@gmail.com.\nYou can also send a email with any questions or suggestions, or to just chat."
-    c = str(easygui.buttonbox(message,"Open MC Manager", ['Start', 'Openfiles', 'Create', 'Remove', 'Import'])).lower()
-    if c == "start":
-        if Start():
-            break
-    elif c == "openfiles":
-        Openfiles()
-    elif c == "create":
+    message = "Welcome to Open MC Manager!\nIf you are thinking, This GUI is crap... You're right. I'm not too good with GUIs, but if you are feel free to contribute on git hub or emailing Wizardkoala31@gmail.com.\nYou can also send a email with any questions or suggestions, or to just chat."
+    NamesRemoved.append("Create");NamesRemoved.append("Import")
+    c = str(easygui.buttonbox(message,"Open MC Manager", NamesRemoved))
+    c = c.replace(" ", "_")
+
+    if c == "Create":
         Build()
-    elif c == "import":
-        easygui.msgbox("This feature is currently not avalible, sorry.")
-    elif c == "remove":
-        Remove()
-    elif c == "none":
+    elif c == "Import":
+        Import()
+
+    elif c in Names:
+        Name = c
+        c = str(easygui.buttonbox("What do you want to do with "+Name.replace("_", " ")+"?", "Open MC Manager", ["Start", "Rename", "Openfiles", "Remove", "Back"]))
+        if c == "Start":
+            Start(Name)
+            break
+        elif c == "Rename":
+            Rename(Name)
+        elif c == "Remove":
+            Remove(Name)
+        elif c == "Openfiles":
+            Openfiles(Name)
+        elif c == "Back":
+            BeLazy = True
+        elif c == "None":
+            break
+        else:
+            easygui.msgbox("Something went wrong!", "Open MC Manager: ERROR 1")
+
+    elif c == "None":
         break
+
     else:
         easygui.msgbox("Something went wrong!", "Open MC Manager: ERROR 0")
